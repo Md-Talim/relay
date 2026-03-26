79a94851803413adf648d43ff2e7c1e13d0da852
Md. Talim <talimmohammad116@gmail.com>
2026-03-26 22:42:59 +0530

    refactor: rename app variable to application in main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,13 +11,13 @@ import (
 func main() {
 	start := time.Now()
 
-	app, err := app.NewApplication(start)
+	application, err := app.NewApplication(start)
 	if err != nil {
 		panic(err)
 	}
-	defer app.DB.Close()
+	defer application.DB.Close()
 
-	routes := app.SetupRoutes()
+	routes := application.SetupRoutes()
 
 	server := &http.Server{
 		Addr:              ":8080",
@@ -25,9 +25,9 @@ func main() {
 		ReadHeaderTimeout: 5 * time.Second,
 	}
 
-	app.Logger.Info("http server starting", "addr", server.Addr)
+	application.Logger.Info("http server starting", "addr", server.Addr)
 	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		app.Logger.Error("http server failed", "err", err)
+		application.Logger.Error("http server failed", "err", err)
 		os.Exit(1)
 	}
 }
